0a11d9ea2576361272a9cd2fead918a1adfac6e0
adappatanushi <tanushiadappa2@gmail.com>
2025-06-30 09:07:44 +0530

    Initial commit: Add number guessing game script

---
+++ number-guessing game.py
@@ -0,0 +1,26 @@
+import random
+
+def number_guessing_game():
+    print("Welcome to the Number Guessing Game!")
+    print("I have selected a number between 1 and 100. Can you guess it?")
+    
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+
+            if guess < secret_number:
+                print("Too low! Try again.")
+            elif guess > secret_number:
+                print("Too high! Try again.")
+            else:
+                print(f"Congratulations! You guessed it in {attempts} attempts!")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+number_guessing_game()
+
